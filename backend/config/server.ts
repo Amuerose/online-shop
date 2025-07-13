66565760fc8b19dfd123e753476ad523d13684e9
Давид <david@Mac.home>
2025-07-13 11:24:26 +0200

    Remove broken submodule and cleanup config

--- backend/config/server.ts
+++ backend/config/server.ts
@@ -2,6 +2,11 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env('APP_KEYS')?.split(',') || ['defaultKey1', 'defaultKey2'],
+    keys: env('APP_KEYS', 'keyA,keyB').split(','),
+  },
+  admin: {
+    auth: {
+      secret: env('JWT_SECRET'),
+    },
   },
 });
